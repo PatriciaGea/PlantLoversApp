bc6506956079498b72841942f522b608956d1075
PatriciaGea <patricia.gea@gmail.com>
2026-06-09 21:45:23 +0200

    colors

--- app/src/main/java/com/example/plantloversapp/MainActivity.kt
+++ app/src/main/java/com/example/plantloversapp/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.plantloversapp
 
+import CategoryRow
+import PlantList
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.plantloversapp.ui.theme.PlantLoversAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,24 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PlantLoversAppTheme {
-               Column {}
+               Column {
+                   Text(
+                       text = "Plant Lovers",
+                       style = MaterialTheme.typography.headlineMedium,
+                       modifier = Modifier.padding(16.dp)
+                   )
+                   CategoryRow()
+                   PlantList()
+               }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlantLoversAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/plantloversapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/plantloversapp/ui/theme/Theme.kt
@@ -9,18 +9,18 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(0xFF81C784)
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val LightColors = lightColorScheme(
+    primary = Primary,
+    secondary = Secondary,
+    background = Backgroundx
+)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -31,7 +31,7 @@ private val LightColorScheme = lightColorScheme(
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
     */
-)
+
 
 @Composable
 fun PlantLoversAppTheme(
@@ -47,7 +47,7 @@ fun PlantLoversAppTheme(
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> lightColorScheme()
     }
 
     MaterialTheme(
